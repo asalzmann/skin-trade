0b14c3d1b6b2ce3db8bb2bcadf908a6449e4c700
Aidan Salzmann <aidan96@gmail.com>
2019-04-28 22:18:48 -0700

    del run txt

--- contract_abi.py
+++ contract_abi.py
@@ -1,7 +1,5 @@
 abi = """[
  {
-   A BIG LIST OF ABI INFO SPREAD ACROSS MULTIPLE DICTS
-   comes from remix 
-   see https://hackernoon.com/ethereum-smart-contracts-in-python-a-comprehensive-ish-guide-771b03990988
+   ABIINFOTODO
  }
 ]"""
